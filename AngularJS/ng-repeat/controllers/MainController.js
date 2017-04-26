cb4db00b7669bc908675a950af259c8e4c5aaea9
mehadi07 <saimum22@gmail.com>
2017-04-26 02:37:10 -0400

    Event Handling

--- AngularJS/ng-repeat/controllers/MainController.js
+++ AngularJS/ng-repeat/controllers/MainController.js
@@ -17,7 +17,44 @@ app.controller('MainController', function MainController($scope) {
             salary: 57000
     },
         ];
-    
+
+    var countries = [
+        {
+            name: "UK",
+            cities: [
+                {
+                    name: "London"
+                }, {
+                    name: "Birmingham"
+                }, {
+                    name: "Manchester"
+                }]
+                    },
+        {
+            name: "USA",
+            cities: [
+                {
+                    name: "Los Angeles"
+                }, {
+                    name: "Chicago"
+                }, {
+                    name: "Houston"
+                }]
+                    },
+        {
+            name: "India",
+            cities: [
+                {
+                    name: "Hyderabad"
+                }, {
+                    name: "Chennai"
+                }, {
+                    name: "Mumbai"
+                }]
+                    }
+                ];
+
     $scope.employees = employees;
-    
+    $scope.countries = countries;
+
 });
