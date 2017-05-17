a033d1b69e5045f43595d63e9f849f5c64d9b69b
mehadi07 <saimum22@gmail.com>
2017-05-16 21:29:13 -0400

    ACME Projects

--- Angular2/ACME Products/app/app.module.ts
+++ Angular2/ACME Products/app/app.module.ts
@@ -1,11 +1,20 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import {enableProdMode} from '@angular/core';
+
+enableProdMode();
+// enabled it by importing and executing the function (before calling bootstrap)
+
+
 
 import { AppComponent }  from './app.component';
 
+import { ProductListComponent } from './products/product-list.component';
+
+
 @NgModule({
   imports: [ BrowserModule ],
-  declarations: [ AppComponent ],
+  declarations: [ AppComponent, ProductListComponent ],
   bootstrap: [ AppComponent ]
 })
 export class AppModule { }
