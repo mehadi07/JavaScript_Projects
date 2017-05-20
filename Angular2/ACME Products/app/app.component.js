bfd30cdae83b1cce6faf1e99c69ee84517d2e218
mehadi07 <saimum22@gmail.com>
2017-05-20 17:42:38 -0400

    services

--- Angular2/ACME Products/app/app.component.js
+++ Angular2/ACME Products/app/app.component.js
@@ -7,6 +7,7 @@ var __decorate = (this && this.__decorate) || function (decorators, target, key,
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 var core_1 = require("@angular/core");
+var product_service_1 = require("./products/product.service");
 var AppComponent = (function () {
     function AppComponent() {
         this.pageTitle = 'ACME Product Management';
@@ -17,7 +18,8 @@ AppComponent = __decorate([
     core_1.Component({
         selector: 'pm-app',
         // not quotes for template
-        template: " \n    <div><h1> {{pageTitle}} </h1>\n            <pm-products></pm-products>\n    </div>"
+        template: " \n    <div><h1> {{pageTitle}} </h1>\n            <pm-products></pm-products>\n    </div>",
+        providers: [product_service_1.ProductService],
     })
 ], AppComponent);
 exports.AppComponent = AppComponent;
